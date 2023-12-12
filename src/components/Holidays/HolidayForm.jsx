7aa8a6f241b043e4545181aea80aa00165785e9d
baharkose <baharkse17@gmail.com>
2023-12-12 18:44:33 +0300

    added form elements

--- src/components/Holidays/HolidayForm.jsx
+++ src/components/Holidays/HolidayForm.jsx
@@ -23,39 +23,95 @@ const HolidayForm = ({ show, setShow, selectedHoliday }) => {
 
   return (
     <>
-    {
-      
-       <Modal show={show} onHide={handleClose}>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Modal.Header closeButton>
-            <Modal.Title>{lokasyon}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {" "}
-            <div>
-              <span>Tarih:</span> {tarih}
-            </div>
-            <div>
-              <span>Süre:</span> {sure}
-            </div>
-            <div>
-              <span>Fiyat:</span> {fiyat}
-            </div>
-
-            
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button type="submit" variant="primary">
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-    }
-     
+      {
+        <Modal show={show} onHide={handleClose}>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+              <Modal.Title>{lokasyon}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>
+                <span className="fw-bold">Tarih:</span> {tarih}
+              </div>
+              <div>
+                <span className="fw-bold">Süre:</span> {sure}
+              </div>
+              <div>
+                <span className="fw-bold">Fiyat:</span> {fiyat}
+              </div>
+              <h4 className="mt-3">Kayıt Formu</h4>
+              <Row className="mb-3">
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom01"
+                  className="d-flex w-90 gap-1 align-items-center"
+                >
+                  <Form.Label>Ad:</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Adınızı giriniz"
+                    defaultValue=""
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Row>
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom02"
+                  className="d-flex w-90 gap-1 align-items-center"
+                >
+                  <Form.Label className="w-20">Soyad:</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Soyadınızı giriniz"
+                    defaultValue=""
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Row>
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustomUsername"
+                  className="d-flex w-90 gap-1 align-items-center"
+                >
+                  <Form.Label>E-Posta Adresi</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Control
+                      type="email"
+                      placeholder="Epostanızı giriniz"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Lütfen geçerli bir e-posta adresi girin.
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Row>
+              <Form.Group className="mb-3">
+                <Form.Check
+                  required
+                  label="Şartlara ve koşullara katılıyorum"
+                  feedback="Devam etmeden önce kabul etmelisiniz."
+                  feedbackType="invalid"
+                />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Kapat
+              </Button>
+              <Button type="submit" variant="primary">
+                Değişiklikleri Kaydet
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      }
     </>
   );
 };
